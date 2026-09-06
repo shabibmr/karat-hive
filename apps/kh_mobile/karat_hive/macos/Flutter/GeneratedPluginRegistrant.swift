ff70bf9ce8930683d8e7aa617b7d14e96abbda47
shabibmr <shabibmr@gmail.com>
2026-09-06 07:48:19 +0530

    feat: initialize Firebase project and configure karat_hive mobile & admin apps

--- apps/kh_mobile/karat_hive/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/kh_mobile/karat_hive/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
+import firebase_core
 import flutter_secure_storage_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
 }
